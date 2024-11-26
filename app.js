757f980fb25806b3316d62fb6453a6e7467d0bb2
Houssem Haji <houssem.haji@insat.ucar.tn>
2024-11-26 14:51:06 +0100

    jenkinsfile update

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello, Kubernetes version 2!!!!!!!');
+    res.send('Hello, Kubernetes version');
 });
 
 app.listen(PORT, () => {
